7f9fb1525b526e52e99346da239154d2f8f2a550
Pahang Krisdyan <pahangkrisdyan@gmail.com>
2025-04-29 20:48:31 +0700

    Update to use docker compose instead of docker-compose

--- certbot.sh
+++ certbot.sh
@@ -7,10 +7,10 @@ DOMAINS=(
 
 # Create certificates for each domain
 for domain in "${DOMAINS[@]}"; do
-    docker-compose run --rm certbot certonly --webroot --webroot-path=/var/www/html --email admin@pahang.ai --agree-tos --no-eff-email --force-renewal -d $domain
+    docker compose run --rm certbot certonly --webroot --webroot-path=/var/www/html --email admin@pahang.ai --agree-tos --no-eff-email --force-renewal -d $domain
 done
 
 # Create a cron job for renewal
-echo "0 0 1 * * docker-compose run --rm certbot renew && docker-compose exec nginx nginx -s reload" > certbot-cron
+echo "0 0 1 * * docker compose run --rm certbot renew && docker compose exec nginx nginx -s reload" > certbot-cron
 crontab certbot-cron
 rm certbot-cron 
